9cb4693c153ebf5c9b321f1fb62b53e390a72e59
t.salehi <tah.salehi.9910@gmail.com>
2022-06-24 13:54:11 +0430

    first commit

---
+++ cal.js
@@ -0,0 +1,42 @@
+// ?clear all the values
+function clear() {
+    document.getElementById("result").value = "";
+}
+ 
+//? display values in result section
+function display(value) {
+   var finalResult = document.getElementById("result").value += value;
+    var p = eval(finalResult)
+    console.log(p);
+}
+ 
+//? calculate and  returns result
+
+// TODO :
+function result() {
+    
+    switch (value) {
+        case 'C':
+            
+            break;
+
+        case '+':
+        var x = document.getElementById("result")
+
+            break;
+        case '-':
+
+            break;
+
+        case '*':
+
+            break;
+    
+        case '=':
+            document.getElementById("result").value = p
+            console.log(document.getElementById("result").value);
+            break;
+            
+    }
+  
+}
